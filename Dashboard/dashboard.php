3fb6fbead85cf411ebfea2369f0ade4ce563f0f4
Gustavo Puga <gustavopuga2008@gmail.com>
2026-03-07 00:01:28 -0300

    Primeira Dashboard

---
+++ Dashboard/dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<title>Dashboard Moderno</title>
+
+</head>
+
+<body>
+
+<div class="container">
+
+<!-- MENU -->
+<div class="sidebar">
+
+<h2>🚀 Meu Painel</h2>
+
+<ul class="menu">
+<li>🏠 Dashboard</li>
+<li>👤 Usuários</li>
+<li>💰 Vendas</li>
+<li>📊 Estatísticas</li>
+<li>⚙️ Configurações</li>
+</ul>
+
+</div>
+
+<!-- CONTEÚDO -->
+<div class="main">
+
+<div class="topbar">
+
+<h1>Dashboard</h1>
+
+<input class="search" placeholder="Pesquisar">
+
+</div>
+
+<div class="cards">
+
+<div class="card">
+<h3>Usuários</h3>
+<p>1.245</p>
+</div>
+
+<div class="card">
+<h3>Vendas</h3>
+<p>R$ 32.500</p>
+</div>
+
+<div class="card">
+<h3>Pedidos</h3>
+<p>320</p>
+</div>
+
+<div class="card">
+<h3>Visitas</h3>
+<p>8.932</p>
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
